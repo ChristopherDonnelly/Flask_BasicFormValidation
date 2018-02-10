57b5f93e4a15d82bfbdb647a8bfa4233983b8457
Chris Donnelly <christopher.d.donnelly@gmail.com>
2018-02-09 16:55:31 -0800

    Commit Basic Form Validation assignment

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, redirect, request, session, flash
+
+app = Flask(__name__)
+
+app.secret_key = 'BasicFormValidationKey'
+
+@app.route('/')
+
+def index():
+    return render_template('index.html')
+
+@app.route('/process', methods=['Post'])
+def process():
+    if len(request.form['name']) < 1:
+        flash("Name cannot be empty!", 'red') # just pass a string to the flash function
+    else:
+        flash("Success! Your name is {}".format(request.form['name'])) # just pass a string to the flash function
+    
+    return redirect('/')
+
+
+app.run(debug=True)
